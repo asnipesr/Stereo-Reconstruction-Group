133b3abf3c47b4529e90614dea36e4ba76a2468d
bn0367 <bnewman@oberlin.edu>
2022-05-19 12:08:10 -0400

    added visualization

--- main.c
+++ main.c
@@ -3,9 +3,16 @@
 #include "match_points.h"
 
 
+typedef struct {
+	double x;
+	double y;
+	double z;
+} vector;
+
 int main(int argc, char *argv[]) {
 	Points* p = malloc(sizeof(Points));
-
+	vector camera1_pos = { 3, 0, 0 };
+	vector camera2_pos = { 0, 0, 0 };
 	get_match_points("..\\..\\..\\images\\view0.png", "..\\..\\..\\images\\view1.png", &p);
 
 	for (int y = 0; y < p->length; y++) {
